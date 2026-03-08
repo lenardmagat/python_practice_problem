a7fa06471a9bf54ab539f6acd1071cc2a0bf866c
lenard magat <lenonmagat8@gmail.com>
2026-03-08 12:48:57 +0800

    program number 23

--- prog08.py
+++ prog08.py
@@ -1,10 +1,17 @@
 #Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 def c_count(string, parameter):
+    char_match = 0
     parameter_match = 0
     for i in range(len(string)):
-        if string[i] == parameter:
-            parameter_match += 1
+        if string[i] == parameter[char_match]:
+            char_match += 1
+            if char_match == len(parameter):
+                parameter_match += 1
+                char_match = 0
+        else:
+            char_match = 0
     return print(parameter_match)
 
-string = input("Enter any string")
-c_count(string, "L")
+string = input("Enter any string: ")
+paramter = "practice"
+c_count(string, paramter)
---
+++ prog23.py
@@ -0,0 +1,3 @@
+#Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+string = input("Enter your full name: ")
+print(f"Your full name is: {string.strip().upper()}")
