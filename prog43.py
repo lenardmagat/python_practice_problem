d9d794209e7ae1d971eec64b58b60ffdbe8253a2
lenard magat <lenonmagat8@gmail.com>
2026-03-19 13:24:37 +0800

    program number 43

---
+++ prog43.py
@@ -0,0 +1,7 @@
+#Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+try:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    print("The sum of two numbers is %d" %(num2 + num1))
+except ValueError:
+    print("Invalid input!")
