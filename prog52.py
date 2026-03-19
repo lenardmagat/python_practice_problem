7e01c4941358b1b2c3d1c6d626ef1b88713d1397
lenard magat <lenonmagat8@gmail.com>
2026-03-19 13:48:56 +0800

    not equal

---
+++ prog52.py
@@ -0,0 +1,10 @@
+#Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+try:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    if num1 != num2:
+        print("Not Equal")
+    else:
+        print("Equal") 
+except ValueError:
+    print("Invalid input!")
